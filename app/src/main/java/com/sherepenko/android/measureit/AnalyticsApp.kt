f83fb4d68dfffeddb36bab07aca2ff50267a57d5
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-05-23 14:40:28 +0300

    Code refactoring [ci skip]

--- app/src/main/java/com/sherepenko/android/measureit/AnalyticsApp.kt
+++ app/src/main/java/com/sherepenko/android/measureit/AnalyticsApp.kt
@@ -1,6 +1,8 @@
 package com.sherepenko.android.measureit
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.sherepenko.android.measureit.providers.HumidityDataSourceImpl
 import com.sherepenko.android.measureit.providers.PressureDataSourceImpl
@@ -12,6 +14,13 @@ import com.sherepenko.android.measureit.repositories.PressureRepositoryImpl
 import com.sherepenko.android.measureit.repositories.TemperatureRepository
 import com.sherepenko.android.measureit.repositories.TemperatureRepositoryImpl
 import com.sherepenko.android.measureit.viewmodels.DashboardViewModel
+import io.reactivex.rxjava3.exceptions.UndeliverableException
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import java.lang.NullPointerException
+import java.net.SocketException
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -55,8 +64,49 @@ class AnalyticsApp : Application() {
 
         AndroidThreeTen.init(this@AnalyticsApp)
 
-        Timber.plant(Timber.DebugTree())
+        setupTimber()
+        setupRx()
+        setupKoin()
+    }
 
+    private fun setupTimber() {
+        Timber.plant(CrashlyticsTree())
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun setupRx() {
+        RxJavaPlugins.setErrorHandler { e ->
+            var error = e
+
+            if (error is UndeliverableException && error.cause != null) {
+                error = error.cause!!
+            }
+
+            if (error is IOException || error is SocketException) {
+                return@setErrorHandler
+            }
+
+            if (error is InterruptedException) {
+                return@setErrorHandler
+            }
+
+            if (error is NullPointerException ||
+                error is IllegalArgumentException ||
+                error is IllegalStateException
+            ) {
+                Thread.currentThread().uncaughtExceptionHandler
+                    ?.uncaughtException(Thread.currentThread(), error)
+                return@setErrorHandler
+            }
+
+            Timber.w(error)
+        }
+    }
+
+    private fun setupKoin() {
         startKoin {
             androidLogger()
             androidContext(this@AnalyticsApp)
@@ -65,6 +115,23 @@ class AnalyticsApp : Application() {
                 repositoryModule,
                 viewModelModule
             )
+        }
+    }
+}
+
+internal class CrashlyticsTree : Timber.Tree() {
+
+    override fun isLoggable(tag: String?, priority: Int): Boolean =
+        priority == Log.WARN || priority == Log.ERROR || priority == Log.ASSERT
+
+    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+        FirebaseCrashlytics.getInstance().apply {
+            log(message)
+
+            throwable?.let {
+                recordException(it)
+                sendUnsentReports()
+            }
         }
     }
 }
